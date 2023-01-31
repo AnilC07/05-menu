6462d98288ea2c19b021602baf558702165443ee
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-01-31 23:30:52 +0100

    complet project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ['all', ...new Set(items.map(item =>{
+  return item.category
+}))]
+
+console.log(allCategories)
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setcategories] = useState([]);
+
+  const filterItems = (category) => {
+    if(category === "all"){
+      setMenuItems(items)
+    }else{
+      const newItems = items.filter(item => item.category === category);
+      setMenuItems(newItems);
+    }
+  }
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={allCategories}  filterItems={filterItems}/>
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
